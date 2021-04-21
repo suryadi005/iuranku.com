1bcf2e78ff0048a81a770f561a58270a47ba8bf1
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2021-04-21 07:31:24 +0700

    init midtrans

--- pkg/manage-group/index.js
+++ pkg/manage-group/index.js
@@ -1,7 +1,14 @@
 const cacheControl = require('express-cache-controller')
+const midtransClient = require('midtrans-client');
 const express = require('express')
 const Group = require('./models/group')
 const router = express.Router()
+
+const LAYANAN_HARGA_MAP = {
+    Netflix: 44000,
+    Spotify: 44000,
+    Youtube: 44000,
+}
 
 function manageGroup (db) {
     // Grub page
@@ -42,11 +49,44 @@ function manageGroup (db) {
             }) 
             const expiredAt = new Date(currentUserOrder ? currentUserOrder.createdAt: undefined);
 
+            let orderPaymentContext = undefined
+
+            if (process.env.MINDTRANS_ENABLED === 'true' && currentUserOrder.status === 'menunggu_pembayaran') {
+                const isProduction = process.env.NODE_ENV === 'production'
+                // initialize snap client object
+                let snap = new midtransClient.Snap({
+                  isProduction : isProduction,
+                  serverKey: process.env.MIDTRANS_SERVER_KEY,
+                  clientKey: process.env.MIDTRANS_CLIENT_KEY,
+                });
+    
+                let parameter = {
+                  "transaction_details": {
+                    "order_id": isProduction ? currentUserOrder.id : new Date().getTime(),
+                    "gross_amount": LAYANAN_HARGA_MAP[currentUserOrder.layanan],
+                  }, "credit_card":{
+                    "secure" : true
+                  }
+                };
+
+                // create snap transaction token
+                const transactionToken = await snap.createTransactionToken(parameter)
+
+                orderPaymentContext = {
+                    token: transactionToken, 
+                    clientKey: snap.apiConfig.clientKey,
+                    snapJsSrc: process.env.MIDTRANS_SNAP_JS_SRC,
+                    grossAmount: LAYANAN_HARGA_MAP[currentUserOrder.layanan]
+                }
+            }
+
+
             expiredAt.setHours(expiredAt.getHours() + 12);
             res.render('pages/group-status', {
                 group,
                 currentUserOrder,
-                expiredAt
+                expiredAt,
+                orderPaymentContext
             });
         }catch(error) {
             error.status = 404
